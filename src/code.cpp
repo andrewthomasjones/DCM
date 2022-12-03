b2ca9d9dd7ace81250826364e2475b842f0aa2c1
Andrew <andrew.jones@qcif.edu.au>
2022-12-03 14:28:35 +1000

    half way done with first pass recoding as cpp

--- src/code.cpp
+++ src/code.cpp
@@ -64,3 +64,160 @@ arma::mat frequencyDistributionCpp(const arma::mat& cold) {
 
   return(fdd);
 }
+
+
+//' @export
+// [[Rcpp::export]]
+List createConcepts(const arma::mat& data_matrix, int nmax_choiceset_size = 31) {
+
+  int nconcepts, nlines_data, ncovariates, nlines_data_matrix;
+  int i1, i2, i4, i5, i6;
+  int iddm, idcs;
+
+  nlines_data_matrix = data_matrix.n_rows;
+  ncovariates = data_matrix.n_cols - 3;
+
+  iddm = data_matrix(0, 0);
+  idcs = data_matrix(0, 1);
+
+  arma::mat data_big(nlines_data_matrix, nmax_choiceset_size + 4, arma::fill::zeros);
+  data_big(0, 0) = iddm;
+
+  if (data_matrix(0, 2) == 1){
+    data_big(0, 1) = 1.0;
+  }
+  data_big(0, 3) = 1.0;
+
+  arma::mat concept_big(nlines_data_matrix,ncovariates, arma::fill::zeros);
+
+  for (int i3=0; i3<ncovariates; i3++){
+    concept_big(0, i3) = data_matrix(0, i3 + 3);
+  }
+  data_big(0, 4) = 1.0;
+
+  // i1 is the count of lines moving through the data matrix
+  // i2 is the build of the number of rows in data_big
+  // i3 is the count of covariates moving through the row of the data matrix
+  // i4 is the count of lines moving through the concept matrix
+  // i5 is the build of the number of concepts in a row in data_big
+  // i6 is build of the number of concepts
+
+  i1 = 1.0;
+  i2 = 1.0;
+  i5 = 1.0;
+  i6 = 1.0;
+
+
+  while (i1 < nlines_data_matrix) {
+    i1 <- i1 + 1
+    match_number <- 0
+    i4 <- 0
+
+    while (i4 < i6) {
+      i4 <- i4 + 1
+      match_count <- 0
+
+      for (i3 in 1:ncovariates) {
+        if (concept_big[i4, i3] == data_matrix[i1, i3 + 3])
+          match_count <- match_count + 1
+      }
+
+      if (match_count == ncovariates) {
+        match_number <- i4
+        i4 <- i6
+      }
+    }
+
+    if (match_number == 0) {
+      i6 <- i6 + 1
+      match_number <- i6
+      for (i3 in 1:ncovariates)
+        concept_big[i6, i3] <- data_matrix[i1, i3 + 3]
+    }
+
+
+    if (data_matrix[i1, 1] == iddm) {
+      if (data_matrix[i1, 2] == idcs) {
+        i5 <- i5 + 1
+        data_big[i2, i5 + 4] <- match_number
+        if (data_matrix[i1, 3] == 1)
+          data_big[i2, 2] <- match_number
+      }
+    }
+
+    if (data_matrix[i1, 1] == iddm) {
+      if (data_matrix[i1, 2] > idcs) {
+#print(data_big[i2,])
+        idcs <- data_matrix[i1, 2]
+        i5 <- 1
+        i2 <- i2 + 1
+        data_big[i2, 1] <- iddm
+        data_big[i2, 4] <- 1
+        data_big[i2, i5 + 4] <- match_number
+        if (data_matrix[i1, 3] == 1)
+          data_big[i2, 2] <- match_number
+      }
+    }
+
+    if (data_matrix[i1, 1] > iddm) {
+      iddm <- data_matrix[i1, 1]
+      idcs <- data_matrix[i1, 2]
+      i5 <- 1
+      i2 <- i2 + 1
+      data_big[i2, 1] <- iddm
+      data_big[i2, 4] <- 1
+      data_big[i2, i5 + 4] <- match_number
+      if (data_matrix[i1, 3] == 1)
+        data_big[i2, 2] <- match_number
+
+    }
+
+
+  }
+
+  concept <- matrix((1:i6 * ncovariates) * 0, i6, ncovariates)
+    for (i8 in 1:i6) {
+      for (i9 in 1:ncovariates) {
+        concept[i8, i9] = concept_big[i8, i9]
+      }
+    }
+
+
+
+
+
+  nconcepts = i6;
+
+  arma::mat data = arma::mat(i2, i2, arma::fill::zeros);
+
+  //replace with a bulk copy
+  for (int i8 = 0; i8 < i2; i8++){
+    for (int i9 = 0; i9 < (nmax_choiceset_size + 4); i9++){
+      data(i8, i9) = data_big(i8, i9);
+      }
+  }
+
+  nlines_data = i2;
+
+  List L = List::create(Named("data") = data,
+                        _["nconcepts"] = nconcepts,
+                        _["nlines_data"] = nlines_data,
+                        _["data_big"] = data_big);
+  return(L);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
