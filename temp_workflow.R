5b0e34ca16ad56a719331f2b1dcde405b3aab9f7
Andrew <andrew.jones@qcif.edu.au>
2023-01-26 19:35:41 +1000

    more tidy

--- temp_workflow.R
+++ temp_workflow.R
@@ -1,11 +1,12 @@
 #library(devtools)
 #devtools::install_git('ssh://git@github.com/andrewthomasjones/DCM.git')
 #install.packages(file.choose(), repos=NULL)
-
+library(tictoc)
+tic()
 library(DCM)
 
 #filename<-'/Users/uqajon14/Downloads/values_data_waves1234.txt'
-filename<-'/Users/uqajon14/Downloads/R Code_RP SP/SP.txt'
+filename<-'/Users/uqajon14/Downloads/R Code_RP SP/RP_SP.txt'
 
 processed<-setUp(filename, header = F)
 
@@ -19,13 +20,13 @@ random_model <- model_generator(ncovariates, npp, nhop, 'Random Model', "Random"
 global_model <- model_generator(ncovariates, npp, nhop, 'Global Model', "Global")
 
 #set up specific models
-SP_fixed_model<-model_generator(ncovariates, npp, nhop, 'SP_FIXED', "Fixed")
-SP_one_factor_model<-model_generator(ncovariates, npp, nhop, 'SP_ONE FACTOR', "Global")
+fixed_model<-model_generator(ncovariates, npp, nhop, 'FIXED', "Fixed")
+one_factor_model<-model_generator(ncovariates, npp, nhop, 'ONE FACTOR', "Global")
 
-runModel(SP_fixed_model, processed, 0, "RP_FIXED 0")
-runModel(SP_fixed_model, processed, 1, "RP_FIXED 1")
+runModel(fixed_model, processed, 0, "FIXED 0")
+runModel(fixed_model, processed, 1, "FIXED 1")
 
-runModel(SP_one_factor_model, processed, 0, "RP_1F 0")
-runModel(SP_one_factor_model, processed, 1, "RP_1F 1")
-
+runModel(one_factor_model, processed, 0, "1F 0")
+runModel(one_factor_model, processed, 1, "1F 1")
+toc()
 
