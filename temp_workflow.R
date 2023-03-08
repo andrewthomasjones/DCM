ed38c9ce825a027f93459de3169be8756d63c50c
Andrew <andrew.jones@qcif.edu.au>
2023-03-08 11:35:59 +1000

    minor clean up and edits

--- temp_workflow.R
+++ temp_workflow.R
@@ -5,8 +5,8 @@ library(tictoc)
 tic()
 library(DCM)
 
-filename<-'/Users/uqajon14/Downloads/values_data_waves1234.txt'
-#filename<-'/Users/uqajon14/Downloads/R Code_RP SP/RP_SP.txt'
+#filename<-'/Users/uqajon14/Downloads/values_data_waves1234.txt'
+filename<-'/Users/uqajon14/Downloads/R Code_RP SP/RP_SP.txt'
 
 processed<-setUp(filename, header = T)
 
