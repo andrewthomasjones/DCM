7379d0fcfc4fb6f04f63f6529f1c803ff4c99182
Andrew <andrew.jones@qcif.edu.au>
2023-03-19 15:12:54 +1000

    added KOBE stuff to workflow

--- temp_workflow.R
+++ temp_workflow.R
@@ -15,10 +15,6 @@ nhop<-1
 #set up default models
 fixed_model <- model_generator(processed, nhop, 'Fixed Model', "Fixed")
 random_model <- model_generator(processed, nhop, 'Random Model', "Random")
-global_model <- model_generator(processed, nhop, 'Global Model', "Global")
-
-#set up specific models
-fixed_model<-model_generator(processed, nhop, 'FIXED', "Fixed")
 one_factor_model<-model_generator(processed, nhop, 'ONE FACTOR', "Global")
 
 
@@ -37,3 +33,41 @@ parPrintOld(r4)
 
 
 
+#
+#
+# dataname=paste("./DATA/",select.list(list.files(path = "./DATA", pattern = ".RData"), multiple = FALSE,
+#                                      title = "Choose your dataset", graphics = TRUE),sep="")
+# load(dataname)
+#
+# EMIs <- as.matrix(select.list(list.files(path = "./EMIs", pattern = "EMI_"), preselect = NULL, multiple = TRUE,
+#                               title = "Model specification (hold CTRL to select multiple", graphics = TRUE))
+#
+# descriptions <- as.matrix(file_path_sans_ext(EMIs))
+# descriptions
+#
+# ndraws <<- as.integer(select.list(c("100","1000","10000"), multiple = FALSE,
+#                                   title = "Specify the resample size", graphics = TRUE))
+#
+# ndraws_save <<- ndraws
+# ll_list <- vector()
+# ncov_list <- vector()
+# nhop_list <- vector()
+# AIC_list <- vector()
+# BIC_list <- vector()
+#
+# for (i in 1:nrow(descriptions)){
+#   description=descriptions[i]
+#
+#   source('./R/KOBEemi.R')
+#   source('./R/KOBEcheckmodel.R')
+#   source('./R/KOBEresample.R')
+#
+#   KOBEcheckmodel(paste('./WORKSPACE/',description,".RData",sep=""))
+# }
+#
+# model_catalogue <- matrix(cbind(c(descriptions,nhop_list,ncov_list,ll_list,AIC_list,BIC_list)),ncol=6)
+# colnames(model_catalogue) <- c("Model","C","K","LL","AIC","BIC")
+# rownames(model_catalogue) <- paste0(rep("M",nrow(model_catalogue)),1:nrow(model_catalogue))
+# print(model_catalogue)
+# write.csv(model_catalogue, paste0("./RESULTS/","RESULTS_CATALOGUE_",format(Sys.time(), "%Y-%m-%d_%H-%M-%S"),".csv"))
+#
