05ce1f11bd153075ff990a0ffd91981cec9d9077
Andrew <andrew.jones@qcif.edu.au>
2023-03-26 15:41:27 +1000

    add some example data(for now)

---
+++ R/example_data.R
@@ -0,0 +1,39 @@
+#' BW Dataset
+#'
+#' Data from ....
+#'
+#' @docType data
+#'
+#' @usage data(BWpriorities)
+#'
+#' @format A dataset in the standard DCE format.
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source
+#'
+#' @examples
+"BWpriorities"
+
+
+#' DCE Dataset
+#'
+#' Data from ....
+#'
+#' @docType data
+#'
+#' @usage data(DCEpriorities)
+#'
+#' @format A dataset in the standard DCE format.
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source
+#'
+#' @examples
+"DCEpriorities"
+
