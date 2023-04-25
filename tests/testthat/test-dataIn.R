99099a01af78d08c61fb48bae8a0f879a52af76f
Andrew Jones <andrew.jones@qcif.edu.au>
2023-04-25 17:31:24 +1000

    comment out test for now

--- tests/testthat/test-dataIn.R
+++ tests/testthat/test-dataIn.R
@@ -1,4 +1,4 @@
 test_that("multiplication works", {
-  useful_thing1 <- readRDS(test_path("fixtures", "useful_thing1.rds"))
+  #useful_thing1 <- readRDS(test_path("fixtures", "useful_thing1.rds"))
   expect_equal(2 * 2, 4)
 })
