f65f73a83969fb2f40ee502c818740726909092e
Andrew Jones <andrew.jones@qcif.edu.au>
2023-04-25 16:38:27 +1000

    parprint and some other stuff works better now remove old code

--- R/parprint.R
+++ R/parprint.R
@@ -1,61 +1,63 @@
 #' @export
-parPrintOld <- function(results) {
+parPrint<- function(results) {
 
   npp <- results$model$npp
   nhop <- results$model$nhop
-  parcountin <- results$par_count
+  parcountin <- parameterCount(results$model)
 
   rr <- results$results
 
 
   resprint <- array(0, parcountin$total + 8)
-  resprint[1] <- paste0("Printout of results for ", results$result_name, "\\\\")
-  resprint[2] <- paste0("log likelihood", round(results$LL, 4), "\\\\")
-  resprint[3] <- paste0("Number of parameters", parcountin$total, "\\\\")
-  resprint[4] <- "$\\\\"
-  resprint[5] <- "\\begin {array} {lrr}"
-  resprint[6] <- paste0("Model Parameter & Estimate & Standard Error\\\\")
-  resprinti <- 6
+  resprint[1] <- paste0("Printout of results for ", results$model_name, "\\\\")
+  resprint[2] <- paste0("Log-likelihood: ", round(results$LL, 4), "\\\\")
+  resprint[3] <- paste0("Number of parameters: ", parcountin$total, "\\\\")
+  resprint[4] <- "\\begin{center}"
+  resprint[5] <- "\\begin{tabular}{lrr}"
+  resprint[6] <- ""
+  resprint[7] <- paste0("Model Parameter & Estimate & Standard Error\\\\")
+  resprint[8] <- paste0("\\hline \\\\")
+  resprinti <- 8
 
   for (i in 1:npp) {
     if (results$model$epsilon[i, 1] == 1) {
       resprinti <- resprinti + 1
-      resprint[resprinti] <- paste0("\\epsilon_ {\\mu, ", i, "}&",
-                                    round(rr$estimate[i], 4), "&",
-                                    round(rr$standard_errors[i], 4), "\\\\")
+      resprint[resprinti] <- paste0("$\\epsilon_ {\\mu, ", i, "}$&$",
+                                    round(rr$estimate[i], 4), "$&$",
+                                    round(rr$standard_errors[i], 4), "$\\\\")
     }
   }
   for (i in 1:nhop) {
     if (results$model$delta[i, 1] == 1) {
       resprinti <- resprinti + 1
-      resprint[resprinti] <- paste0("\\delta_ {\\mu, ", i, "}&",
-                                    round(rr$estimate[i], 4), "&",
-                                    round(rr$standard_errors[i], 4), "\\\\")
+      resprint[resprinti] <- paste0("$\\delta_ {\\mu, ", i, "}$&$",
+                                    round(rr$estimate[i], 4), "$&$",
+                                    round(rr$standard_errors[i], 4), "$\\\\")
     }
   }
   for (i in 1:npp) {
     if (results$model$epsilon[i, 2] == 1) {
       resprinti <- resprinti + 1
-      resprint[resprinti] <- paste0("\\epsilon_ {\\sigma, ", i, "}&",
-                                    round(rr$estimate[i], 4), "&",
-                                    round(rr$standard_errors[i], 4), "\\\\")
+      resprint[resprinti] <- paste0("$\\epsilon_ {\\sigma, ", i, "}$&$",
+                                    round(rr$estimate[i], 4), "$&$",
+                                    round(rr$standard_errors[i], 4), "$\\\\")
     }
   }
   for (i in 1:nhop) {
     if (results$model$delta[i, 2] == 1) {
       resprinti <- resprinti + 1
-      resprint[resprinti] <- paste0("\\delta_ {\\sigma, ", i, "}&",
-                                    round(rr$estimate[i], 4), "&",
-                                    round(rr$standard_errors[i], 4), "\\\\")
+      resprint[resprinti] <- paste0("$\\delta_ {\\sigma, ", i, "}$&$",
+                                    round(rr$estimate[i], 4), "$&$",
+                                    round(rr$standard_errors[i], 4), "$\\\\")
     }
   }
   for (i in 1:npp) {
     for (j in 1:nhop) {
       if (results$model$gamma[i, j] == 1) {
         resprinti <- resprinti + 1
-        resprint[resprinti] <- paste0("\\gamma_ {", i, ", ", j, "}&",
-                                      round(rr$estimate[i], 4), "&",
-                                      round(rr$standard_errors[i], 4), "\\\\")
+        resprint[resprinti] <- paste0("$\\gamma_ {", i, ", ", j, "}$&$",
+                                      round(rr$estimate[i], 4), "$&$",
+                                      round(rr$standard_errors[i], 4), "$\\\\")
       }
     }
   }
@@ -63,9 +65,9 @@ parPrintOld <- function(results) {
     for (j in 1:nhop) {
       if (results$model$beta[i, j] == 1) {
         resprinti <- resprinti + 1
-        resprint[resprinti] <- paste0("\\beta_ {", i, ", ", j, "}&",
-                                      round(rr$estimate[i], 4), "&",
-                                      round(rr$standard_errors[i], 4), "\\\\")
+        resprint[resprinti] <- paste0("$\\beta_ {", i, ", ", j, "}$&$",
+                                      round(rr$estimate[i], 4), "$&$",
+                                      round(rr$standard_errors[i], 4), "$\\\\")
       }
     }
   }
@@ -74,18 +76,18 @@ parPrintOld <- function(results) {
       if (results$model$phi[i, j] == 1) {
         if (results$model$phi[j, i] == 1) {
           resprinti <- resprinti + 1
-          resprint[resprinti] <- paste0("\\phi_ {", i, ", ", j, "}&",
-                                        round(rr$estimate[i], 4), "&",
-                                        round(rr$standard_errors[i], 4), "\\\\")
+          resprint[resprinti] <- paste0("$\\phi_ {", i, ", ", j, "}$&$",
+                                        round(rr$estimate[i], 4), "$&$",
+                                        round(rr$standard_errors[i], 4), "$\\\\")
         }
       }
     }
   }
   resprinti <- resprinti + 1
-  resprint[resprinti] <- "\\end {array}"
+  resprint[resprinti] <- "\\end{tabular}"
   resprinti <- resprinti + 1
-  resprint[resprinti] <- "$\\\\"
+  resprint[resprinti] <- "\\end{center}"
 
-
-  return(resprint)
+  out <- paste(resprint, sep="\n")
+  return(out)
 }
