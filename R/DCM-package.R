b571eb3e1410c2c7195bb2d34a972c72d856392c
Andrew Jones <andrew.jones@qcif.edu.au>
2023-09-30 17:31:25 +1000

    minor updates/renaming

--- R/DCM-package.R
+++ R/DCM-package.R
@@ -1,10 +1,12 @@
 ## usethis namespace: start
+#' @importFrom data.table as.data.table
 #' @importFrom lifecycle deprecated
+#' @importFrom mltools one_hot
 #' @importFrom Rcpp sourceCpp
-#' @importFrom tools file_ext
 #' @importFrom stats na.omit
 #' @importFrom stats nlm
 #' @importFrom stats qnorm
+#' @importFrom tools file_ext
 #' @importFrom utils read.table read.csv
 ## usethis namespace: end
 NULL
