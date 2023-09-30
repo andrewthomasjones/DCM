132b7ac46ebf4a22c5447f1e98b8deb3a2fcaef7
Andrew Jones <andrew.jones@qcif.edu.au>
2023-09-30 15:32:31 +1000

    fixed up some man page stuff and also salvage the ASCII art

--- R/menu.R
+++ R/menu.R
@@ -1,16 +1,37 @@
-.onAttach <- function(...) {
-  header <- cli::rule(
-    left = cli::style_bold("Discrete Choice Modeling in R"),
-    right = paste0("DCM ", "0.1.2")
-  )
-  inform_startup(paste0(header, "\n"), paste0(c("Can", "also", "print", "more"), sep="\n"))
-
+inform_startup <- function()
+{
+  msg <- c(paste0(
+    "    ###################################################################################
+    #KKKKKKKKK    KKKKKKK     OOOOOOOOO     BBBBBBBBBBBBBBBBB   EEEEEEEEEEEEEEEEEEEEEE#
+    #K:::::::K    K:::::K   OO:::::::::OO   B::::::::::::::::B  E::::::::::::::::::::E#
+    #K:::::::K    K:::::K OO:::::::::::::OO B::::::BBBBBB:::::B E::::::::::::::::::::E#
+    #K:::::::K   K::::::KO:::::::OOO:::::::OBB:::::B     B:::::BEE::::::EEEEEEEEE::::E#
+    #KK::::::K  K:::::KKKO::::::O   O::::::O  B::::B     B:::::B  E:::::E       EEEEEE#
+    #K:::::K K:::::K   O:::::O     O:::::O  B::::B     B:::::B  E:::::E               #
+    #K::::::K:::::K    O:::::O     O:::::O  B::::BBBBBB:::::B   E::::::EEEEEEEEEE     #
+    #K:::::::::::K     O:::::O     O:::::O  B:::::::::::::BB    E:::::::::::::::E     #
+    #K:::::::::::K     O:::::O     O:::::O  B::::BBBBBB:::::B   E:::::::::::::::E     #
+    #K::::::K:::::K    O:::::O     O:::::O  B::::B     B:::::B  E::::::EEEEEEEEEE     #
+    #K:::::K K:::::K   O:::::O     O:::::O  B::::B     B:::::B  E:::::E               #
+    #KK::::::K  K:::::KKKO::::::O   O::::::O  B::::B     B:::::B  E:::::E       EEEEEE#
+    #K:::::::K   K::::::KO:::::::OOO:::::::OBB:::::BBBBBB::::::BEE::::::EEEEEEEE:::::E#
+    #K:::::::K    K:::::K OO:::::::::::::OO B:::::::::::::::::B E::::::::::::::::::::E#
+    #K:::::::K    K:::::K   OO:::::::::OO   B::::::::::::::::B  E::::::::::::::::::::E#
+    #KKKKKKKKK    KKKKKKK     OOOOOOOOO     BBBBBBBBBBBBBBBBB   EEEEEEEEEEEEEEEEEEEEEE#
+    ###################################################################################
+    version ",
+    packageVersion("DCM")),
+    "\nType 'citation(\"DCM\")' for citing this R package in publications.")
+  return(msg)
 }
 
-inform_startup <- function(msg, ...) {
-  if (is.null(msg)) {
-    return()
-  }
 
-  rlang::inform(msg, ..., class = "packageStartupMessage")
+.onAttach <- function(lib, pkg)
+{
+
+  msg <- inform_startup()
+  packageStartupMessage(msg)
+  invisible()
 }
+
+
