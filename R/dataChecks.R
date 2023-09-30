1e8b7fea460c482c3582d1c78bfb9417c455078b
Andrew Jones <andrew.jones@qcif.edu.au>
2023-09-30 17:32:27 +1000

    added VIF - is this necessary also maybe remove stuff about dummy vars

--- R/dataChecks.R
+++ R/dataChecks.R
@@ -1,7 +1,37 @@
-
-
-
 #precheck for multicolinearity
+
+
+#' choice_model_vif
+#' @param processed_data processed data list
+#' @returns joins two processed objects
+#' @export
+choice_model_vif  <-  function(processed_data) {
+  #does this even make sense to do
+
+  dataset <- processed_data$data_original[,4:ncol(processed_data$data_original)]
+  factors <- names(dataset)[sapply(dataset, is.factor)]
+
+  if(length(factors)>0){
+
+    dataset <- mltools::one_hot(data.table::as.data.table(dataset))
+  }
+
+  columns <- 1:ncol(dataset)
+
+  vif <- data.frame(Variable = names(dataset), VIF = NA)
+
+  for (i in columns){
+
+    f <- paste(names(dataset)[i], "~.")
+    lm1 <- lm(f, data=dataset)
+    vif1 <- 1/(1-summary(lm1)$r.squared)
+    vif$VIF[i]<-vif1
+  }
+
+
+   return(vif)
+}
+
 
 
 #other set up checks from older models
