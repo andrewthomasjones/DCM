3f13f989d522c74b6aaa4952a52bf3a4ae461fe2
Andrew Jones <andrew.jones@qcif.edu.au>
2024-05-12 15:51:12 +1000

    more sims code

--- Simulation_workflow2.R
+++ Simulation_workflow2.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(doParallel)
 registerDoParallel(cores=6)
 
-# chosen_values <- list()
+chosen_values <- list()
 # colvars <- 2
 # chosen_values[["fixed"]][["DCE"]] <- c(1.2, 1.4)
 # chosen_values[["fixed"]][["BW"]] <- c(1.8, 0.7)
@@ -21,26 +21,33 @@ registerDoParallel(cores=6)
 # chosen_values[["mtmm"]][["BW"]] <- NA
 # chosen_values[["mtmm"]][["BWDCE"]] <- c(3.0,  1.9,  3.0,  0.5, 2.5,  1.5,  1.5, -1.5,  2.7,  2.2,  2.1,  0.6,  0.9, -0.5)
 
-data_sets <- c("DCE") #DCE", "BW", "BWDCE")
+data_sets <- c("DCE", "BW", "BWDCE")
 integral_types <- c("TMB", "draws", "ghq")
 
 precision_levels <- list()
 precision_levels[["draws"]] <- c(1000)
 precision_levels[["TMB"]] <- c(0)
 
-n_sims <-  1
+n_sims <-  600
 m_list <- c(200) #, 100, 250)
-models <- c("one-factor",  "random", "fixed") #, "mtmm")
+models <- c("one-factor",  "random", "fixed", "mtmm")
 precision_levels[["ghq"]] <- c(3)
 
 chosen_values <- list()
-#for (colvars in seq(2,7)) {
-  colvars <- 4
-  #filename <- paste0("./TESTING_DUMP/par_test_20240512_DCE_", colvars, ".Rdata")
+
+for (colvars in seq(2,7)) {
+
+  filename <- paste0("./TESTING_DUMP/par_test_20240512_", colvars, ".Rdata")
 
   processedDCE <- setUp(DCEpriorities)
   processedDCE <- select_variables(processedDCE , processedDCE$attribute_names[seq_len(colvars)])
 
+  processedBW <- setUp(BWpriorities)
+  processedBW <- select_variables(processedBW , processedBW$attribute_names[seq_len(colvars)])
+
+  processedBWDCE <- join_choicedatasets(processedBW, processedDCE)
+
+  ###################################
   model_1f <- model_generator(processedDCE, "one-factor")
   model_fixed <- model_generator(processedDCE, "fixed")
   model_random <- model_generator(processedDCE, "random")
@@ -53,39 +60,92 @@ chosen_values <- list()
   chosen_values[["fixed"]][["DCE"]] <- res_fixed$results$estimate
   chosen_values[["random"]][["DCE"]] <- res_random$results$estimate
 
+  ###################################
+  model_1f <- model_generator(processedBW, "one-factor")
+  model_fixed <- model_generator(processedBW, "fixed")
+  model_random <- model_generator(processedBW, "random")
+
+  res_1f <- runModel(model_1f)
+  res_fixed <- runModel(model_fixed)
+  res_random <- runModel(model_random)
+
+  chosen_values[["one-factor"]][["BW"]] <- res_1f$results$estimate
+  chosen_values[["fixed"]][["BW"]] <- res_fixed$results$estimate
+  chosen_values[["random"]][["BW"]] <- res_random$results$estimate
+
+  ###################################
+  model_1f <- model_generator(processedBWDCE, "one-factor")
+  model_fixed <- model_generator(processedBWDCE, "fixed")
+  model_random <- model_generator(processedBWDCE, "random")
+  model_mtmm <- model_generator(processedBWDCE, "mtmm")
+
+  res_1f <- runModel(model_1f)
+  res_fixed <- runModel(model_fixed)
+  res_random <- runModel(model_random)
+  res_mtmm <- runModel(model_mtmm)
+
+  chosen_values[["one-factor"]][["BWDCE"]] <- res_1f$results$estimate
+  chosen_values[["fixed"]][["BWDCE"]] <- res_fixed$results$estimate
+  chosen_values[["random"]][["BWDCE"]] <- res_random$results$estimate
+  chosen_values[["mtmm"]][["BWDCE"]] <- res_mtmm$results$estimate
+
+  chosen_values[["mtmm"]][["DCE"]] <- NA
+  chosen_values[["mtmm"]][["BW"]] <- NA
+
   big_list <- run_sims(data_sets, chosen_values, precision_levels, integral_types, models, m_list, n_sims, colvars, filename)
+}
 
-#}
+# res<-list()
+# i<-1
+#
+# for (colvars in seq(2,7)) {
+#
+#   filename <- paste0("./TESTING_DUMP/par_test_20240512_", colvars, ".Rdata")
+#
+#   gc()
+#   load(file=filename)
+#   sim_results <- process_sims(big_list, params$data_sets, params$chosen_values, params$precision_levels, params$integral_types, params$models, params$m_list, params$n_sims,  0.95, params$colvars)
+#   sim_results$n_cols <- colvars
+#
+#   i <- i + 1
+#   res[[i]] <- sim_results
+# }
+#
+# results <- bind_rows(res)
 
 
 
-  res<-list()
 
-  colvars <- 2
-  filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
-  load(file=filename)
-  print(colvars)
-  res[[1]] <- process_sims(big_list, params$data_sets, params$chosen_values, params$precision_levels, params$integral_types, params$models, params$m_list, params$n_sims)
-  res[[1]]$cols <- 2
 
-  colvars <- 3
-  filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
-  load(file=filename)
-  print(colvars)
-  res[[2]] <- process_sims(big_list, params$data_sets, params$chosen_values, params$precision_levels, params$integral_types, params$models, params$m_list, params$n_sims)
-  res[[2]]$cols <- 3
-
-  # colvars <- 4
-  # filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
-  # load(file=filename)
-  # print(colvars)
-  # res[[3]] <- process_sims(big_list, params$data_sets, params$chosen_values[c(1,3)], params$precision_levels, params$integral_types, params$models[1:2], params$m_list, params$n_sims)
-
-  all_BW <- bind_rows(res)
-
-  all_BW %>% filter(cols==3) %>% filter(model_type == "random") %>% ggplot(aes(x=name, colour=integral_type, shape = start , y=coverage_probability)) +
-    geom_point(size=2) + facet_grid(cols = vars(data_type), scales="free_x") + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + geom_hline(yintercept=0.95, linetype="dashed")
-
+#
+#
+#
+#
+#   colvars <- 2
+#   filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
+#   load(file=filename)
+#   print(colvars)
+#   res[[1]] <- process_sims(big_list, params$data_sets, params$chosen_values, params$precision_levels, params$integral_types, params$models, params$m_list, params$n_sims)
+#   res[[1]]$cols <- 2
+#
+#   colvars <- 3
+#   filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
+#   load(file=filename)
+#   print(colvars)
+#   res[[2]] <- process_sims(big_list, params$data_sets, params$chosen_values, params$precision_levels, params$integral_types, params$models, params$m_list, params$n_sims)
+#   res[[2]]$cols <- 3
+#
+#   # colvars <- 4
+#   # filename <- paste0("./TESTING_DUMP/par_test_20240422_BW_", colvars, ".Rdata")
+#   # load(file=filename)
+#   # print(colvars)
+#   # res[[3]] <- process_sims(big_list, params$data_sets, params$chosen_values[c(1,3)], params$precision_levels, params$integral_types, params$models[1:2], params$m_list, params$n_sims)
+#
+#   all_BW <- bind_rows(res)
+#
+#   all_BW %>% filter(cols==3) %>% filter(model_type == "random") %>% ggplot(aes(x=name, colour=integral_type, shape = start , y=coverage_probability)) +
+#     geom_point(size=2) + facet_grid(cols = vars(data_type), scales="free_x") + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + geom_hline(yintercept=0.95, linetype="dashed")
+#
 
 
 
